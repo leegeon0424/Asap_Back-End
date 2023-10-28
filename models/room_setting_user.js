041de3420d85fbad0b8feaaf3f3623ae97d2dc25
leegeon <baegoodbae1@naver.com>
2023-10-28 15:55:01 +0900

    이건/룸 세팅 유저 만듬

---
+++ models/room_setting_user.js
@@ -0,0 +1,29 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+    class Room_setting_user extends Model {
+        /**
+         * Helper method for defining associations.
+         * This method is not a part of Sequelize lifecycle.
+         * The `models/index` file will call this method automatically.
+         */
+        static associate(models) {
+            // define association here
+        }
+    }
+    Room_setting_user.init(
+        {
+            room_id: DataTypes.INTEGER,
+            setting_id: DataTypes.INTEGER,
+            user_id: DataTypes.INTEGER,
+        },
+        {
+            sequelize,
+            tableName: 'room_setting_users',
+            modelName: 'Room_setting_user',
+            timestamps: true,
+            charset: 'utf8',
+        }
+    );
+    return Room_setting_user;
+};
