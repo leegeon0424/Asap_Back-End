1467b94b1834a3f49be2225f68d40914a4177002
TAESUNG LEE <ts.lee@vtpl.kr>
2023-10-28 16:24:00 +0900

    이태성/associate와 index 변경

--- models/user.js
+++ models/user.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      this.hasMany(models.Room_setting_user)
     }
   }
   User.init({
